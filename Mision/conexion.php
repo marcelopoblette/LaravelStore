48a44d23f390e2ebde29801c09a3b31bfafd1150
L. Marcelo Poblete Sandoval <marcelopoblette@gmail.com>
2020-07-21 22:06:16 -0400

    downloding Mission

--- Mision/conexion.php
+++
@@ -1,3 +0,0 @@
-<!-- Conexión a la base de datos,
-codificación de caracteres,
-seleccion de base de datos. -->
--- MisionPHP7/salir.php
+++ MisionPHP7/salir.php
@@ -2,3 +2,13 @@
 <!-- Verificar que la variable sal sea igual a si.
 Cerrar la sesión. 
 Redirigir el flujo a la pagina del login --> 
+
+<?php
+
+if ($_GET['sal']=='si') {
+
+	session_start();
+	session_destroy();
+	header("Location:login.php");
+}
+?>
